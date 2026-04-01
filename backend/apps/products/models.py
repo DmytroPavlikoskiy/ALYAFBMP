e9a535ea8df2ff9f474269329b14763391d247ae
maksimkarbovnik <getuch375@gmail.com>
2026-04-01 18:53:10 +0300

    add modles User, category and UserPreference

---
+++ backend/apps/products/models.py
@@ -0,0 +1,32 @@
+from common.database import Base
+import uuid
+from datetime import datetime
+from typing import List, Optional
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+class UserPreference(Base):
+    __tablename__ = "user_preferences"
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id", on_delete="CASCADE"), 
+        primary_key=True
+    )
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id", on_delete="CASCADE"), 
+        primary_key=True
+    )
+
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    icon_url: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+
+    interested_users: Mapped[List["User"]] = relationship(
+        secondary="user_preferences", 
+        back_populates="preferences"
+    )
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,7 +1,28 @@
 #Опис БД моделі
 from common.database import Base
-# from sqlalchemy import String, Integer, ...
+import uuid
+from datetime import datetime
+from typing import List, Optional
+from sqlalchemy import String, ForeignKey, DateTime, func, text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
+class Base(DeclarativeBase):
+    pass
 
 class User(Base):
-    pass
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        primary_key=True, 
+        default=uuid.uuid4,
+        server_default=text("gen_random_uuid()")
+    )
+    email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
+    role: Mapped[str] = mapped_column(String(20), server_default="USER")
+    
+    # Зв'язок Many-to-Many через допоміжну таблицю
+    preferences: Mapped[List["Category"]] = relationship(
+        secondary="user_preferences", 
+        back_populates="interested_users"
+    )
